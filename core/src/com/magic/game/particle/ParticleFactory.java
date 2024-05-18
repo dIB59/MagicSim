4fd7d25e394f6970062544b2bea8b9047a59dd3a
Ibrahim <Ibrahim@ebs.net.pk>
2024-05-18 21:47:21 +0200

    add factory

---
+++ core/src/com/magic/game/particle/ParticleFactory.java
@@ -0,0 +1,69 @@
+package com.magic.game.particle;
+
+import com.badlogic.gdx.graphics.Color;
+import com.magic.game.physics.MovableSpatialElement;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+
+public class ParticleFactory {
+    private static final Random RANDOM = new Random();
+
+    public static List<MovableSpatialElement> createMany(int numberOfParticles) {
+        List<MovableSpatialElement> particles = new ArrayList<>();
+
+        for (int i = 0; i < numberOfParticles; i++) {
+            particles.add(createOne());
+        }
+        return particles;
+    }
+
+    public static Particle createOne() {
+        final ParticleBuilder builder = new ParticleBuilder();
+
+        Color randomColor = getRandomColor();
+        int randomMass = getRandomMass();
+        int randomRadius = getRandomRadius();
+        int randomVelX = getRandomVelocity();
+        int randomVelY = getRandomVelocity();
+        int randomX = getRandomPosition();
+        int randomY = getRandomPosition();
+
+        builder.setColor(randomColor)
+                .setMass(randomMass)
+                .setRadius(randomRadius)
+                .setVelX(randomVelX)
+                .setVelY(randomVelY)
+                .setX(randomX)
+                .setY(randomY);
+
+        return builder.createParticle();
+    }
+
+    private static Color getRandomColor() {
+        float r = RANDOM.nextFloat();
+        float g = RANDOM.nextFloat();
+        float b = RANDOM.nextFloat();
+        return new Color(r, g, b, 1);
+    }
+
+    private static int getRandomMass() {
+        return RANDOM.nextInt(10) + 1;
+    }
+
+    private static int getRandomRadius() {
+        return RANDOM.nextInt(16) + 5;
+    }
+
+    private static int getRandomVelocity() {
+        return RANDOM.nextInt(11) - 5;
+    }
+
+    private static int getRandomPosition() {
+        return RANDOM.nextInt(800);
+    }
+
+}
+
