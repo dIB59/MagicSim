226b9fd84d048ccc323c6fd9f80b50fdfd7dd78c
Ibrahim <Ibrahim@ebs.net.pk>
2024-05-19 18:52:49 +0200

    refactor simulation logic

---
+++ core/src/main/java/com/magic/game/simulation/Grid.java
@@ -0,0 +1,45 @@
+package com.magic.game.simulation;
+
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Grid {
+    @Getter
+    private final int gridWidth;
+    @Getter
+    private final int gridHeight;
+    @Getter
+    private final List<Cell> cells;
+//    private final Map<Integer, List<Cell>> surroundingCellsCache;
+
+    public Grid(int gridWidth, int gridHeight, int numOfCells) {
+        this.gridWidth = gridWidth;
+        this.gridHeight = gridHeight;
+        this.cells = new ArrayList<>();
+//        this.surroundingCellsCache = surroundingCellsCache;
+    }
+
+    public int getCellSize(){
+        return this.cells.size();
+    }
+
+//    public void addElement(MovableSpatialElement element) {
+//        int cellX = (int) (element.getX() / cellSize);
+//        int cellY = (int) (element.getY() / cellSize);
+//    }
+
+//    public List<List<MovableSpatialElement>> getSurroundingCells(int cellX, int cellY) {
+//        int key = getKey(cellX * cellSize, cellY * cellSize);
+//        return surroundingCellsCache.get(key);
+//    }
+
+//    private List<List<MovableSpatialElement>> computeSurroundingCells(int cellX, int cellY) {
+//
+//    }
+
+//    private int getKey(int x, int y) {
+//        return y * gridWidth + x;
+//    }
+}
