0cf1f11b2fb6fe98148ba97247a1060825de7d0a
Ibrahim <Ibrahim@ebs.net.pk>
2024-05-19 02:13:26 +0200

    update imports

--- core/src/main/java/com/magic/game/MagicSim.java
+++ core/src/main/java/com/magic/game/MagicSim.java
@@ -3,11 +3,8 @@ package com.magic.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import com.badlogic.gdx.utils.ScreenUtils;
 import com.magic.game.particle.ParticleFactory;
 import com.magic.game.physics.MovableSpatialElement;
 import com.magic.game.simulation.Simulation;
@@ -23,7 +20,7 @@ public class MagicSim extends ApplicationAdapter {
     public void create() {
         batch = new SpriteBatch();
         shapeRenderer = new ShapeRenderer();
-        simulation = new Simulation(ParticleFactory.createMany(20),Gdx.graphics.getWidth(), Gdx.graphics.getHeight() ,30, 0.1f);
+        simulation = new Simulation(ParticleFactory.createMany(200),Gdx.graphics.getWidth(), Gdx.graphics.getHeight() ,30, 0.1f);
 
     }
 
