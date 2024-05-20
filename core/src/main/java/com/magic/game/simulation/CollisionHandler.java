b94a653984a03b0109f9d0a1b43b55861970f0ce
Ibrahim <Ibrahim@ebs.net.pk>
2024-05-20 17:41:41 +0200

    overhauled collisions

--- core/src/main/java/com/magic/game/simulation/CollisionHandler.java
+++ core/src/main/java/com/magic/game/simulation/CollisionHandler.java
@@ -6,40 +6,9 @@ import com.magic.game.physics.SpatialElement;
 
 public interface CollisionHandler {
 
-    default void resolveCollision(MovableSpatialElement element, MovableSpatialElement particle) {
-        double dx = particle.getX() - element.getX();
-        double dy = particle.getY() - element.getY();
-        double squaredDistance = dx * dx + dy * dy;
-        int combinedRadius = element.getBoundary() + particle.getBoundary();
-        int squaredRadius = combinedRadius * combinedRadius;
+    void resolveCollision(MovableSpatialElement element, MovableSpatialElement particle, float timestep);
 
-        if (!(squaredDistance >= squaredRadius)) {
-            return;
-        }
-
-        double angle = Math.atan2(dy, dx);
-
-        double totalMass = element.getMass() + particle.getMass();
-
-        element.setVelocityX(((element.getMass() - particle.getMass()) / totalMass) * element.getVelocityX() + (2 * particle.getMass() / totalMass)
-                * particle.getVelocityX() * element.getVelocityDampner());
-        element.setVelocityY(((element.getMass() - particle.getMass()) / totalMass) * element.getVelocityY() + (2 * particle.getMass() / totalMass)
-                * particle.getVelocityY() * element.getVelocityDampner());
-        particle.setVelocityX((2 * element.getMass() / totalMass) * element.getVelocityX() - ((element.getMass() - particle.getMass()) / totalMass)
-                * particle.getVelocityX() * element.getVelocityDampner());
-        particle.setVelocityY((2 * element.getMass() / totalMass) * element.getVelocityY() - ((element.getMass() - particle.getMass()) / totalMass)
-                * particle.getVelocityY() * element.getVelocityDampner());
-
-        double overlap = combinedRadius - Math.sqrt(squaredDistance);
-        double moveX = overlap * Math.cos(angle);
-        double moveY = overlap * Math.sin(angle);
-        element.setXCoordinate((float) (element.getX() - (moveX / 2)));
-        element.setYCoordinate((float) (element.getY() - (moveY / 2)));
-        particle.setXCoordinate((float) (particle.getX() + (moveX / 2)));
-        particle.setYCoordinate((float) (particle.getY() + (moveY / 2)));
-    }
-
-    default void resolveCollision(SpatialElement element, MovableSpatialElement particle) {
+    default void resolveCollision(SpatialElement element, MovableSpatialElement particle, float timestep) {
         double dx = element.getX() - particle.getX();
         double dy = element.getY() - particle.getY();
         double squaredBoundary = dx * dx + dy * dy;
@@ -49,8 +18,8 @@ public interface CollisionHandler {
             return;
         }
 
-        particle.setXCoordinate((float) (-particle.getX() * particle.getVelocityDampner()));
-        particle.setYCoordinate((float) (-particle.getY() * particle.getVelocityDampner()));
+        particle.setX((float) (-particle.getX() * particle.getVelocityDampner()));
+        particle.setY((float) (-particle.getY() * particle.getVelocityDampner()));
     }
 
     boolean isColliding(SpatialElement element, SpatialElement element2);
